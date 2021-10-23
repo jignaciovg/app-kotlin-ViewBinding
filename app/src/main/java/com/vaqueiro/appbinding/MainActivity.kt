297bb4faafc8e9c894211b9e93f2a038affaba32
jignaciovg <ignaciovaqdevelop@hotmail.com>
2021-10-23 09:01:12 -0500

    implementacion y uso del viewBinding

--- app/src/main/java/com/vaqueiro/appbinding/MainActivity.kt
+++ app/src/main/java/com/vaqueiro/appbinding/MainActivity.kt
@@ -4,18 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.vaqueiro.appbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        /*FORMA TRADICIONAL
         setContentView(R.layout.activity_main)
-        
-        //FORMA TRADICIONAL
         var boton = findViewById<Button>(R.id.btnCentro)
         boton.setOnClickListener{
             Toast.makeText(this,"EVENTO PUSH BOTON", Toast.LENGTH_LONG).show()
-        }
+        }*/
 
+        //IMPLEMENTACION DE VIEW_BINDING
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnCentro.setOnClickListener{
+            Toast.makeText(this,"EVENTO PUSH BOTON", Toast.LENGTH_LONG).show()
+        }
 
 
     }
